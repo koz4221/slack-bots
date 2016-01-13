e823084a384cce68fdf824cc960d7ba55dc00e97
koz <koz4221@yahoo.com>
2016-01-13 04:15:02 +0000

    convo testing

--- certbot.js
+++ certbot.js
@@ -42,4 +42,34 @@ controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function
 
 controller.hears(['i love you'],'direct_message,direct_mention,mention',function(bot, message) {
     bot.reply(message,"Aw, I love you too! :heart:")
-})
+});
+
+controller.hears('poll','direct_message,direct_mention',function(bot, message) {
+  bot.startConversation(message, function(err, convo) {
+    if (err) { console.log(err); }
+    convo.ask("Poll time! What do you want to poll?", [
+      {
+        pattern: 'quit',
+        callback: function(response,convo) {
+          convo.say("OK, no more poll.");
+          convo.next();
+        }
+      },
+      {
+        default: true,
+        callback: function(response,convo) {
+          convo.say("got here");
+          convo.stop();
+        }
+      }
+    ]);
+  });
+});
+
+controller.hears('debug','direct_message',function(bot, message) {
+  //console.log(message.user);
+  bot.api.channels.info({ channel:'C0H8K3WES' },function(err,response) {
+    if (err) { console.log(err); }
+    console.log(response);
+  });
+});
