8fe9551b5fee061fb93d6838fc1ab3010c867137
koz <koz4221@yahoo.com>
2016-01-14 14:48:26 +0000

    certbot updates

--- certbot.js
+++ certbot.js
@@ -35,6 +35,9 @@ var bot = controller.spawn(
   }
 ).startRTM();
 
+var booActivePoll = false;
+var results, options, pollChannel, pollUser;
+var resultsUsers = [];
 
 controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function(bot,message) {
   bot.reply(message,"Hello!");
@@ -45,31 +48,93 @@ controller.hears(['i love you'],'direct_message,direct_mention,mention',function
 });
 
 controller.hears('poll','direct_message,direct_mention',function(bot, message) {
-  bot.startConversation(message, function(err, convo) {
-    if (err) { console.log(err); }
-    convo.ask("Poll time! What do you want to poll?", [
-      {
-        pattern: 'quit',
-        callback: function(response,convo) {
-          convo.say("OK, no more poll.");
-          convo.next();
+  if (message.text == 'poll results') {
+    if (booActivePoll == true) {
+      pollResults(message);
+    }
+    else {
+      bot.reply(message,"There is no poll active.");
+    }
+  }
+  else {
+    bot.startConversation(message, function(err, convo) {
+      if (err) { console.log(err); }
+      convo.ask("Poll time! What do you want to poll?", [
+        {
+          pattern: new RegExp(/^(quit|exit)/i),
+          callback: function(response,convo) {
+            convo.say('OK, no more poll.');
+            convo.next();
+          }
+        },
+        {
+          default: true,
+          callback: function(response,convo) {
+            // just repeat the question
+            var question = response.text;
+            convo.next();
+            convo.ask('OK! Now what are the poll options? Comma-delimited list, please!', function(response,convo) {
+              options = response.text.split(',');
+              
+              var phrase = question;
+              for (var i = 0; i < options.length; i++) {
+                phrase += '\n' + (i + 1) + '. ' + options[i];
+              }
+              phrase += '\n' + 'Type "vote #" to submit your choice!'
+              
+              booActivePoll = true;
+              pollChannel = message.channel;
+              pollUser = message.user;
+              convo.say(phrase);
+              convo.next();
+            });
+          }
         }
-      },
-      {
-        default: true,
-        callback: function(response,convo) {
-          convo.say("got here");
-          convo.stop();
-        }
-      }
-    ]);
-  });
+      ]);
+    });    
+  }
+  
+
 });
 
+controller.hears('vote','direct_message,direct_mention,mention,ambient',function(bot,message) {
+  //console.log(booActivePoll);
+  if (booActivePoll == true && message.text.match(/vote \d+/i) && resultsUsers.indexOf(message.user) == -1) {
+    var choice = message.text.substr(5);
+    
+    if (results === undefined) {
+      results = [];
+    }
+    
+    if (results[choice] === undefined) {
+      results[choice] = 1;
+    }
+    else {
+      results[choice]++;
+    }
+    
+    resultsUsers.push(message.user);
+  }
+});
+
+function pollResults(message) {
+  var phrase = 'Poll results!';
+  for (var i = 0; i < options.length; i++) {
+    phrase += '\n' + options[i] + ' - ' + ((results[i+1] !== undefined) ? results[i+1] : 0);
+  }
+  
+  bot.reply(message,phrase);
+  options = [];
+  results = [];
+  resultsUsers = [];
+  pollChannel, pollUser = {};
+  booActivePoll = false;  
+}
+
 controller.hears('debug','direct_message',function(bot, message) {
-  //console.log(message.user);
+  console.log(message);
   bot.api.channels.info({ channel:'C0H8K3WES' },function(err,response) {
     if (err) { console.log(err); }
-    console.log(response);
+    //console.log(response);
   });
 });
